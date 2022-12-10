493f6995dfc89a1b5517136dcf8e65b44883f1dd
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2022-12-10 10:40:00 -0800

    WIP: getting input from form in javascript UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,22 @@ Pizza.prototype.totalCost = function() {
 }
 
 //UI Logic
-
+function makePizza(event) {
+  event.preventDefault();
+  const newPizza = new Pizza;
+  const sizeInput = document.querySelector("input[name=size]:checked").value;
+  const sauceInput = document.querySelector("input[name=sauce]:checked").value;
+  const meatInput = document.querySelector("input[name=meat]:checked").value;
+  const veggiesInput = document.querySelectorAll("input[name=veggies]:checked");
+  const toppingsInput = document.querySelectorAll("input[name=toppings]:checked");
+  const veggiesArray = Array.from(veggiesInput);
+  const toppingsArray = Array.from(toppingsInput)
+  console.log(sizeInput);
+  console.log(sauceInput);
+  console.log(meatInput);
+  console.log(veggiesArray);
+  console.log(toppingsArray);
+}
 
 function resetForm(event) {
   event.preventDefault();
@@ -44,5 +59,5 @@ function resetForm(event) {
 }
 window.addEventListener("load", function() {
   document.querySelector("form").addEventListener("submit", makePizza);
-  this.document.querySelector("form.output").addEventListener("reset", resetForm);
+  document.querySelector("form").addEventListener("reset", resetForm);
   });
